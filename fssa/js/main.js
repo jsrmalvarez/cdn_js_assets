45c98a76cae3115eb42a1ca1ca617a9a75ddedc2
José Ramón Álvarez Rojo <jsrmalvarez@gmail.com>
2026-05-14 20:13:50 +0200

    Update main.js

--- fssa/js/main.js
+++ fssa/js/main.js
@@ -1,1 +1,7 @@
+import { initQuizzes } from "./quiz.js";
 
+initQuizzes();
+
+if (document.documentElement.dataset.cookieConsentEnabled === "true") {
+  void import("./cookie-consent.js").then((m) => m.initCookieConsent());
+}
