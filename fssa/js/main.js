4f83485bfd66afb0f4b71deba8e4ec3a1b761fc1
José Ramón Álvarez Rojo <jsrmalvarez@gmail.com>
2026-05-14 20:36:47 +0200

    Delete fssa/js/main.js

--- fssa/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initQuizzes } from "./quiz.js";
-
-initQuizzes();
-
-if (document.documentElement.dataset.cookieConsentEnabled === "true") {
-  void import("./cookie-consent.js").then((m) => m.initCookieConsent());
-}
